c8c0254a9402af175ae91f0abf9198275c12e1fc
lukevonbergen <luke_vb1@outlook.com>
2025-01-05 20:21:10 +0000

    service role

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your Supabase project URL and API key
 const supabaseUrl = 'https://xjznwqvwlooarskroogf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhqem53cXZ3bG9vYXJza3Jvb2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MTI4NzQsImV4cCI6MjA1MTM4ODg3NH0.eID9MO_INcBJOnJSm_uBPE-PVMuHQfW1_hWCE-3ihzY';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhqem53cXZ3bG9vYXJza3Jvb2dmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNTgxMjg3NCwiZXhwIjoyMDUxMzg4ODc0fQ.MpzksCX7a-0y_TgpgcAYc-wvB2JcWOzGcfpE8P2UgUM';
 
 // Initialize the Supabase client
 const supabase = createClient(supabaseUrl, supabaseKey);
