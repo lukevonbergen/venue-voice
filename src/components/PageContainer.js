3060d4b12ad1025b74c3a731309bbffb057adda7
lukevonbergen <luke_vb1@outlook.com>
2025-05-14 20:22:39 +0100

    7xl max width

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,9 +1,10 @@
 const PageContainer = ({ children }) => {
     return (
-      <div className="max-w-4xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
         {children}
       </div>
     );
   };
   
   export default PageContainer;
+  
