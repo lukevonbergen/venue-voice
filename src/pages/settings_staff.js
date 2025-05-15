3a98cd319d1dabb1b63842bc5bc778a37db9a57b
lukevonbergen <luke_vb1@outlook.com>
2025-05-15 21:05:32 +0100

    supabase import fix

--- src/pages/settings_staff.js
+++ src/pages/settings_staff.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import supabase from '../../utils/supabase';
+import supabase from '../utils/supabase';
 import Papa from 'papaparse';
 import { useVenue } from '../../context/VenueContext';
 
