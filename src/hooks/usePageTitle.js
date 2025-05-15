e3a1a836ba782e0c5836223cbe6702ad61a91279
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-05-15 15:08:29 +0100

    Update usePageTitle.js

--- src/hooks/usePageTitle.js
+++ src/hooks/usePageTitle.js
@@ -1,1 +1,9 @@
+import { useEffect } from 'react';
 
+const usePageTitle = (pageName) => {
+  useEffect(() => {
+    document.title = `${pageName} – Chatters`;
+  }, [pageName]);
+};
+
+export default usePageTitle;
