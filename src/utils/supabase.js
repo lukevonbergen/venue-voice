57f059f6dca3daa8f279bd1b225132eaf803f44a
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-27 10:08:57 +0000

    move logs

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
 console.log("REACT_APP_SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
 console.log("REACT_APP_SUPABASE_ANON_KEY:", process.env.REACT_APP_SUPABASE_ANON_KEY);
-
-import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
