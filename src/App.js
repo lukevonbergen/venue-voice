b6241c6583aed119128f20fec4443b80159a9211
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-27 07:08:05 +0000

    fix: Missing toast import

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ModalProvider } from './context/ModalContext';
 import { Toaster } from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 
 import LandingPage from './pages/LandingPage';
 import 'antd/dist/reset.css';
