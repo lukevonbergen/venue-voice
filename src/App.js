e956af0617a00a3b67d9a1de9d3781c2b8e5c492
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-27 09:47:07 +0000

    last supabase changeh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import supabase from './utils/supabase';
+import { supabase } from './utils/supabase';
 
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
